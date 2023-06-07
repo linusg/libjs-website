4924eba9b51074b59ea350998b0d295e821e23cf
BuggieBot <buggiebot@serenityos.org>
2023-06-08 00:25:57 +0200

    Deploying to main from @ SerenityOS/serenity@cbe5aeb917eaa96909b4ccf2b41a145b8e3ab00b 🚀

--- repl/libjs.js
+++ repl/libjs.js
@@ -5281,4 +5281,4 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
 
   if (typeof Module != 'undefined' && typeof document != 'undefined') emrun_register_handlers();
 }
-Module.SERENITYOS_COMMIT = "cbe5aeb917eaa96909b4ccf2b41a145b8e3ab00b";
+Module.SERENITYOS_COMMIT = "1367809142ae42d8c73464cc8cceaa7cad772d86";
