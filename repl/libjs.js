128ef44f7809c8cdf4b705f18a3db0d57d4a7bbe
BuggieBot <buggiebot@serenityos.org>
2023-07-03 17:13:26 +0000

    Deploying to main from @ SerenityOS/serenity@21260ea2efcd45996f5b96eb177a6fad1dec6a57 🚀

--- repl/libjs.js
+++ repl/libjs.js
@@ -4306,8 +4306,8 @@ var dynCall_viiji = Module["dynCall_viiji"] = createExportWrapper("dynCall_viiji
 /** @type {function(...*):?} */
 var dynCall_jiji = Module["dynCall_jiji"] = createExportWrapper("dynCall_jiji");
 
-var ___start_em_js = Module['___start_em_js'] = 10475316;
-var ___stop_em_js = Module['___stop_em_js'] = 10475407;
+var ___start_em_js = Module['___start_em_js'] = 10475268;
+var ___stop_em_js = Module['___stop_em_js'] = 10475359;
 function invoke_iiii(index,a1,a2,a3) {
   var sp = stackSave();
   try {
@@ -4900,4 +4900,4 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-Module.SERENITYOS_COMMIT = "8ba4036bba34329754b933241b5f2922bc1cab7a";
+Module.SERENITYOS_COMMIT = "21260ea2efcd45996f5b96eb177a6fad1dec6a57";
